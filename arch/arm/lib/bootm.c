20ba66d81595cb9de36d169fe10c20e3d4e5b1e9
Conke Hu <conke.hu@maxwit.com>
2026-06-10 19:58:47 +0800

    fix coding style
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- arch/arm/lib/bootm.c
+++ arch/arm/lib/bootm.c
@@ -316,13 +316,13 @@ static void boot_jump_linux(bootm_headers_t *images, int flag)
 	else {
 		r2 = gd->bd->bi_boot_params;
 #ifdef CONFIG_OF_CONTROL
-        r2 += 0xc00000; // FIXME
+		r2 += 0xc00000; // FIXME
 #endif
 	}
 
 #ifdef CONFIG_OF_CONTROL
-    r2 = getenv_ulong("fdtcontroladdr", 16, r2);
-    memcpy((void *)r2, gd->fdt_blob, fdt_totalsize(gd->fdt_blob));
+	r2 = getenv_ulong("fdtcontroladdr", 16, r2);
+	memcpy((void *)r2, gd->fdt_blob, fdt_totalsize(gd->fdt_blob));
 #endif
 
 	if (!fake) {
