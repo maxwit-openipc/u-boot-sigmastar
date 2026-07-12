34e7ffec2bd6fae13fde4ae34cb0e5092d1ab464
Conke Hu <conke.hu@maxwit.com>
2026-07-12 11:59:32 +0800

    add boot prompt for FDT
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- common/bootm.c
+++ common/bootm.c
@@ -256,11 +256,12 @@ int bootm_find_ramdisk_fdt(int flag, int argc, char * const argv[])
 #elif defined(CONFIG_OF_CONTROL)
 	images.ft_addr = map_sysmem(gd->fdt_blob, 0);
 	if (fdt_check_header(images.ft_addr)) {
-		printf("Invalid FDT blob at 0x%lx (0x%lx)\n",
-			(ulong)gd->fdt_blob, (ulong)images.ft_addr);
+		printf("Invalid FDT blob at 0x%p\n", images.ft_addr);
 		return -EINVAL;
 	}
 	images.ft_len = fdt_totalsize(images.ft_addr);
+	printf("## FDT blob at 0x%p\n", images.ft_addr);
+	printf("   FDT size: %ld bytes\n\n", images.ft_len);
 #endif
 
 	return 0;
